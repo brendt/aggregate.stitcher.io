d65707f4db5e8193324bcabbf75eba3065b71973
Brent Roose <brent@spatie.be>
2019-01-17 10:51:30 +0100

    Add horizon

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\ViewServiceProvider::class,
 
