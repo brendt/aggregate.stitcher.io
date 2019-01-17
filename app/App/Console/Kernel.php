dab54d3d3059f7b2c5e27acab0cceeb9ff987186
Brent Roose <brent@spatie.be>
2019-01-17 11:18:14 +0100

    Schedule sync command

--- app/App/Console/Kernel.php
+++ app/App/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\SyncSourcesCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -12,6 +13,10 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
+        $schedule
+            ->command(SyncSourcesCommand::class)
+            ->everyMinute()
+            ->withoutOverlapping(60);
     }
 
     protected function commands()
