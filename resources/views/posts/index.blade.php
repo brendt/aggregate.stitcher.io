f3cbc96f65a3fb8c235bb615f9d40e857d1269a2
Brent Roose <brent@spatie.be>
2019-01-21 16:21:23 +0100

    Add unread button

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,46 +8,56 @@
 @component('layouts.app', [
     'title' => $title,
 ])
-    <div class="flex items-baseline mt-6">
-        @isset($currentSource)
-            <heading class="mr-3" nowrap="nowrap">
-                {{ $currentSource->website }}
-            </heading>
-        @endisset
+    <div class="flex items-baseline mt-6 justify-between">
+        <div class="flex">
+            @isset($currentSource)
+                <heading class="mr-3" nowrap="nowrap">
+                    {{ $currentSource->website }}
+                </heading>
+            @endisset
 
-        @isset($currentTopic)
-            <heading class="mr-3" nowrap="nowrap">
-                {{ $currentTopic->name }}
-            </heading>
-        @endisset
+            @isset($currentTopic)
+                <heading class="mr-3" nowrap="nowrap">
+                    {{ $currentTopic->name }}
+                </heading>
+            @endisset
 
-        @isset($currentTag)
-            <p class="text-sm">
-                <tag :tag="$currentTag"></tag>
-            </p>
+            @isset($currentTag)
+                <p class="text-sm">
+                    <tag :tag="$currentTag"></tag>
+                </p>
 
-            @if ($user)
-                @if(! $user->hasMuted($currentTag))
-                    <post-button
-                        :action="$currentTag->getMuteUrl()"
-                        class="ml-2"
-                    >
-                    <span class="
-                        underline
-                        text-grey text-xs
-                        hover:no-underline
-                    ">
-                        {{ __('Mute tag') }}
-                    </span>
-                    </post-button>
+                @if ($user)
+                    @if(! $user->hasMuted($currentTag))
+                        <post-button
+                            :action="$currentTag->getMuteUrl()"
+                            class="ml-2"
+                        >
+                        <span class="
+                            underline
+                            text-grey text-xs
+                            hover:no-underline
+                        ">
+                            {{ __('Mute tag') }}
+                        </span>
+                        </post-button>
+                    @endif
                 @endif
-            @endif
-        @endisset
+            @endisset
 
-        @if(!isset($currentTag) && !isset($currentTopic))
-            <heading nowrap="nowrap">
-                {{ $title }}
-            </heading>
+            @if(!isset($currentTag) && !isset($currentTopic))
+                <heading nowrap="nowrap">
+                    {{ $title }}
+                </heading>
+            @endif
+        </div>
+
+        @if($user)
+            <div class="text-sm">
+                <a href="{{ filter('unread') }}" class="button button-small {{ filter_active('unread') ? 'button-active' : '' }}">
+                    {{ __('Unread') }}
+                </a>
+            </div>
         @endif
     </div>
 
