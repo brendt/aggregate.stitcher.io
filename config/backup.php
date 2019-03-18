96e642bfacb9a1c4923a824eab4199d2863cfa93
Brent Roose <brent@spatie.be>
2019-03-18 15:34:45 +0100

    Enable DB backups

--- config/backup.php
+++ config/backup.php
@@ -82,7 +82,7 @@ return [
         'notifiable' => \Spatie\Backup\Notifications\Notifiable::class,
 
         'mail' => [
-            'to' => 'freek@spatie.be',
+            'to' => 'brent@stitcher.be',
         ],
 
         'slack' => [
