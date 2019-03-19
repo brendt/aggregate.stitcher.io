a4144f9e7461e6600b72e41f71f24d233ddd3e83
Brent Roose <brent@spatie.be>
2019-03-19 14:43:23 +0100

    Improve TweetPost command

--- app/App/Console/Commands/TweetPostCommand.php
+++ app/App/Console/Commands/TweetPostCommand.php
@@ -8,26 +8,39 @@ use Illuminate\Console\Command;
 
 class TweetPostCommand extends Command
 {
-    protected $signature = 'tweet:post';
+    protected $signature = 'tweet:post {uuid?}';
 
     public function handle(
         TweetAction $tweetAction
     ): void {
         $viewThreshold = 20;
 
-        $posts = Post::query()
-            ->whereNotTweeted()
-            ->where('view_count', '>', $viewThreshold)
-            ->where('created_at', '>', now()->subDays(4)->startOfDay()->toDateTimeString())
-            ->get();
+        if ($this->argument('uuid')) {
+            $post = Post::query()
+                ->whereNotTweeted()
+                ->where('uuid', $this->argument('uuid'))
+                ->first();
+        } else {
+            $posts = Post::query()
+                ->whereNotTweeted()
+                ->where('view_count', '>', $viewThreshold)
+                ->where('created_at', '>', now()->subDays(4)->startOfDay()->toDateTimeString())
+                ->get();
 
-        if ($posts->isEmpty()) {
+            if ($posts->isEmpty()) {
+                $this->comment('No post found');
+
+                return;
+            }
+
+            $post = $posts->random();
+        }
+
+        if (! $post) {
             $this->comment('No post found');
 
             return;
         }
-
-        $post = $posts->random();
 
         $this->info("Tweeting post {$post->uuid}");
 
