80b0e7317ac76a528b528c9234d7b658b2ceeba8
Brent Roose <brent@spatie.be>
2019-03-19 14:33:56 +0100

    Improve RSS validation

--- app/Domain/Source/Actions/ValidateSourceAction.php
+++ app/Domain/Source/Actions/ValidateSourceAction.php
@@ -4,6 +4,8 @@ namespace Domain\Source\Actions;
 
 use Domain\Source\Models\Source;
 use Exception;
+use Illuminate\Support\Str;
+use PHPHtmlParser\Dom;
 use Spatie\QueueableAction\QueueableAction;
 use Zend\Feed\Reader\Reader;
 
@@ -16,20 +18,9 @@ final class ValidateSourceAction
 
     public function execute(Source $source): void
     {
-        $host = parse_url($source->url, PHP_URL_HOST);
-
-        $feedUrls = $this->getFormats($host);
-
-        array_unshift(
-            $feedUrls,
-            parse_url($source->url, PHP_URL_HOST) . parse_url($source->url, PHP_URL_PATH)
-        );
-
-        $scheme = parse_url($source->url, PHP_URL_SCHEME);
+        $feedUrls = $this->getPossibleUrls($source->url);
 
         foreach ($feedUrls as $feedUrl) {
-            $feedUrl = "{$scheme}://{$feedUrl}";
-
             try {
                 Reader::import($feedUrl);
 
@@ -48,32 +39,72 @@ final class ValidateSourceAction
         $source->markAsInvalid();
     }
 
-    private function getFormats(string $host): array
+    private function getPossibleUrls(string $url): array
     {
-        return [
-            "{$host}/feed.xml",
-            "{$host}/index.xml",
-            "{$host}/atom.xml",
-            "{$host}/rss.xml",
+        $scheme = parse_url($url, PHP_URL_SCHEME);
 
-            "{$host}/feed",
-            "{$host}/index",
-            "{$host}/atom",
-            "{$host}/rss",
+        $host = parse_url($url, PHP_URL_HOST);
 
-            "{$host}/blog/feed.xml",
-            "{$host}/blog/index.xml",
-            "{$host}/blog/atom.xml",
-            "{$host}/blog/rss.xml",
+        return array_filter([
+            $this->urlFromContent($url),
+            $url,
+            "{$scheme}://{$host}/feed.xml",
+            "{$scheme}://{$host}/index.xml",
+            "{$scheme}://{$host}/atom.xml",
+            "{$scheme}://{$host}/rss.xml",
 
-            "{$host}/blog/feed",
-            "{$host}/blog/index",
-            "{$host}/blog/atom",
-            "{$host}/blog/rss",
-            "{$host}/blog/feed.atom",
+            "{$scheme}://{$host}/feed",
+            "{$scheme}://{$host}/index",
+            "{$scheme}://{$host}/atom",
+            "{$scheme}://{$host}/rss",
 
-            "feed.{$host}",
-            "rss.{$host}",
-        ];
+            "{$scheme}://{$host}/blog/feed.xml",
+            "{$scheme}://{$host}/blog/index.xml",
+            "{$scheme}://{$host}/blog/atom.xml",
+            "{$scheme}://{$host}/blog/rss.xml",
+
+            "{$scheme}://{$host}/blog/feed",
+            "{$scheme}://{$host}/blog/index",
+            "{$scheme}://{$host}/blog/atom",
+            "{$scheme}://{$host}/blog/rss",
+            "{$scheme}://{$host}/blog/feed.atom",
+
+            "{$scheme}://feed.{$host}",
+            "{$scheme}://rss.{$host}",
+        ]);
+    }
+
+    private function urlFromContent(string $url): ?string
+    {
+        $scheme = parse_url($url, PHP_URL_SCHEME);
+        $html = @file_get_contents($url);
+
+        if (! $html) {
+            $host = parse_url($url, PHP_URL_HOST);
+
+            $html = @file_get_contents("{$scheme}://{$host}");
+        }
+
+        if (! $html) {
+            return null;
+        }
+
+        $dom = new Dom();
+
+        $dom->load($html);
+
+        [$link] = $dom->find('head link[type="application/rss+xml"]');
+
+        if (! $link || ! $link->href) {
+            return null;
+        }
+
+        $href = $link->href;
+
+        if (! Str::startsWith($href, 'http')) {
+            $href = $scheme . '://' . preg_replace('/^[\/]+/', '', $href);
+        }
+
+        return $href;
     }
 }
