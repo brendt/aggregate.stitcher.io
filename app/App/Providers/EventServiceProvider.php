250055a647be170abfe47d49724e7ec5b45e41d5
Brent Roose <brent@spatie.be>
2019-03-20 12:21:18 +0100

    Remove TweetSubscriber

--- app/App/Providers/EventServiceProvider.php
+++ app/App/Providers/EventServiceProvider.php
@@ -3,14 +3,12 @@
 namespace App\Providers;
 
 use App\Subscribers\PageCacheSubscriber;
-use App\Subscribers\TweetSubscriber;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
     protected $subscribe = [
         PageCacheSubscriber::class,
-        TweetSubscriber::class,
     ];
 
     public function boot(): void
