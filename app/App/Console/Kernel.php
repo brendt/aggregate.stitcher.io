5082e968ef6e92222545575a7418287ecb17526d
Brent Roose <brent@spatie.be>
2021-03-19 09:15:46 +0100

    wip

--- app/App/Console/Kernel.php
+++ app/App/Console/Kernel.php
@@ -16,24 +16,24 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule): void
     {
-        $schedule
-            ->command(SyncSourcesCommand::class)
-            ->twiceDaily();
-
-        $schedule
-            ->command(SyncPostCountCommand::class)
-            ->daily()
-            ->withoutOverlapping();
-
-        foreach (['01:02', '07:08', '12:49', '19:10'] as $hour) {
-            $schedule
-                ->command(TweetPostCommand::class)
-                ->dailyAt($hour);
-        }
-
-        $schedule
-            ->command(SyncPopularityIndexCommand::class)
-            ->daily();
+//        $schedule
+//            ->command(SyncSourcesCommand::class)
+//            ->twiceDaily();
+//
+//        $schedule
+//            ->command(SyncPostCountCommand::class)
+//            ->daily()
+//            ->withoutOverlapping();
+//
+//        foreach (['01:02', '07:08', '12:49', '19:10'] as $hour) {
+//            $schedule
+//                ->command(TweetPostCommand::class)
+//                ->dailyAt($hour);
+//        }
+//
+//        $schedule
+//            ->command(SyncPopularityIndexCommand::class)
+//            ->daily();
     }
 
     protected function commands(): void
