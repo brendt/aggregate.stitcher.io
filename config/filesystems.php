4a6c4ad3a47f59d286af7ed5b205334d84216449
Brent Roose <brent.roose@gmail.com>
2022-04-14 06:46:36 +0200

    wip

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,6 +36,12 @@ return [
             'throw' => false,
         ],
 
+        'backup' => [
+            'driver' => 'local',
+            'root' => storage_path('backup'),
+            'throw' => false,
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
