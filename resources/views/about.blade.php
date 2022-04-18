86e8c4022aa34980311c81040432dd82e9b5c283
Brent Roose <brent.roose@gmail.com>
2022-04-18 14:00:01 +0200

    wip

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,9 +7,18 @@
         </div>
 
         <div class="bg-white mx-4 shadow-md grid">
-            <div class="px-12 p-4 grid gap-4">
+            <div class="px-12 p-4 grid gap-2">
                 <p>
-                    Welcome to aggregate!
+                    Welcome to Aggregate, a community-driven content aggregator.
+                    It works like this: everyone is able to
+                    <a class="underline hover:no-underline" href="{{ action(\App\Http\Controllers\Sources\SuggestSourceController::class) }}">suggest</a>
+                    a blog or RSS feed.
+                    Its posts are manually curated by <a class="underline hover:no-underline" href="https://twitter.com/brendt_gd">me</a>, together with all other feeds into one
+                    <a class="underline hover:no-underline" href="/">public feed</a>.
+                </p>
+
+                <p>
+                    Only the best content ends up in the public feed, allowing you to browse a high-quality feed, without having to do manual filtering.
                 </p>
             </div>
         </div>
