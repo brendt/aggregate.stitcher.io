2659aba95dc1f4de2d9fb35c324133fe503090c1
Brent Roose <brent.roose@gmail.com>
2022-12-03 07:00:32 +0100

    wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,6 +39,7 @@
                             href="{{ $post->getPublicUrl() }}"
                             class="
                         block lg:px-12 p-4
+                        hover:bg-pink-100
                         {{ $post->isPending() ? 'bg-gray-200' : '' }}
                         {{ $post->isStarred() ? 'bg-yellow-100' : '' }}
                         {{ "hover:bg-{$hoverColor}" }}
