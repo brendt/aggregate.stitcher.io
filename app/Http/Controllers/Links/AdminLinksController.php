ed39ab44ff92520d4a49b22311d5eaf886274fc4
Brent Roose <brent.roose@gmail.com>
2023-01-29 13:31:36 +0100

    wip

--- app/Http/Controllers/Links/AdminLinksController.php
+++ app/Http/Controllers/Links/AdminLinksController.php
@@ -10,24 +10,14 @@ final class AdminLinksController
 {
     public function __invoke(Request $request)
     {
-//        SELECT *
-//        FROM links
-//        LEFT OUTER JOIN (
-//                SELECT link_id, COUNT(*) AS visits_this_week FROM link_visits
-//            WHERE created_at_day >= "2023-01-22"
-//            GROUP BY link_id
-//            HAVING visits_this_week > 50
-//        ) AS lc ON lc.link_id = links.id
-//        ORDER BY
-//            visits_this_week DESC,
-//            created_at DESC
-//
+        $thisWeek = now()->subWeek();
+
         $links = collect(DB::select(DB::raw(<<<SQL
                 SELECT *
                 FROM links
                 LEFT OUTER JOIN (
                         SELECT link_id, COUNT(*) AS visits_this_week FROM link_visits
-                    WHERE created_at_day >= "2023-01-22"
+                    WHERE created_at_day >= "$thisWeek"
                     GROUP BY link_id
                     HAVING visits_this_week > 50
                 ) AS lc ON lc.link_id = links.id
