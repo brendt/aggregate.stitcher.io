540833af6c69e5b9a780f9625429d0b07bc51d91
Brent Roose <brent.roose@gmail.com>
2023-01-30 12:53:58 +0100

    wip

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,9 +14,9 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command(SourceSyncCommand::class)->everyFifteenMinutes();
         $schedule->command(TwitterSyncCommand::class)->everyFiveMinutes();
-        $schedule->command(TweetRandomPostCommand::class)->mondays()->at('11:01');
-        $schedule->command(TweetRandomPostCommand::class)->wednesdays()->at('10:26');
-        $schedule->command(TweetRandomPostCommand::class)->fridays()->at('11:10');
+//        $schedule->command(TweetRandomPostCommand::class)->mondays()->at('11:01');
+//        $schedule->command(TweetRandomPostCommand::class)->wednesdays()->at('10:26');
+//        $schedule->command(TweetRandomPostCommand::class)->fridays()->at('11:10');
     }
 
     protected function commands()
