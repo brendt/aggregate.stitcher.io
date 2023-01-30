65c823b39c1343aa5402daec672beb06295628f5
Brent Roose <brent.roose@gmail.com>
2023-01-30 06:02:05 +0100

    Schedule tweet command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\SourceSyncCommand;
+use App\Console\Commands\TweetRandomPostCommand;
 use App\Console\Commands\TwitterSyncCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -13,6 +14,9 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command(SourceSyncCommand::class)->everyFifteenMinutes();
         $schedule->command(TwitterSyncCommand::class)->everyFiveMinutes();
+        $schedule->command(TweetRandomPostCommand::class)->mondays()->at('11:01');
+        $schedule->command(TweetRandomPostCommand::class)->wednesdays()->at('10:26');
+        $schedule->command(TweetRandomPostCommand::class)->fridays()->at('11:10');
     }
 
     protected function commands()
