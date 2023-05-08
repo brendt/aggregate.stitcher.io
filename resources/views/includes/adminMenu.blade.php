7286128317a1d7ed592e100a71893af5b45ecd82
Brent Roose <brent.roose@gmail.com>
2023-05-08 14:53:10 +0200

    wip

--- resources/views/includes/adminMenu.blade.php
+++ resources/views/includes/adminMenu.blade.php
@@ -1,11 +1,11 @@
-<div class="text-sm text-center text-gray-400 flex justify-center">
+<div class="text-sm text-center text-gray-400 px-4 flex justify-between md:justify-center">
     <a
-        class="mr-2 md:mr-4 underline hover:no-underline text-center"
+        class="md:mr-4 underline hover:no-underline text-center"
         href="{{ action(\App\Http\Controllers\HomeController::class) }}"
     >
         Home</a>
     <a
-        class="mr-2 md:mr-4 underline hover:no-underline text-center"
+        class="md:mr-4 underline hover:no-underline text-center"
         href="{{ action(\App\Http\Controllers\Posts\AdminPostsController::class) }}"
     >
         Posts
@@ -13,22 +13,23 @@
 
     <a
         href="{{ action(\App\Http\Controllers\Sources\AdminSourcesController::class) }}"
-        class="mr-2 md:mr-4 underline hover:no-underline text-center"
+        class="md:mr-4 underline hover:no-underline text-center"
     >
         Sources
         <span class="font-bold">({{ $pendingSources }})</span></a>
 
     <a
-        class="mr-2 md:mr-4 underline hover:no-underline text-center"
+        class="md:mr-4 underline hover:no-underline text-center"
         href="{{ action(\App\Http\Controllers\Posts\FindPostController::class) }}"
     >
         Find</a>
 
     <a
         href="{{ action(\App\Http\Controllers\Links\AdminLinksController::class) }}"
-        class="mr-2 md:mr-4 underline hover:no-underline text-center"
+        class="md:mr-4 underline hover:no-underline text-center hidden md:inline"
     >
         Links</a>
+
     <a
         class="underline hover:no-underline text-center font-bold"
         href="{{ action(\App\Http\Controllers\Users\SendInviteController::class) }}"
