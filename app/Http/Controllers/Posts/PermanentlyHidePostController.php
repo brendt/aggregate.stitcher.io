664014df77c463e4ab721d037005edd6bf4899b7
Brent Roose <brent.roose@gmail.com>
2023-05-19 10:50:48 +0200

    wip

--- app/Http/Controllers/Posts/PermanentlyHidePostController.php
+++ app/Http/Controllers/Posts/PermanentlyHidePostController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers\Posts;
 
 use App\Models\Post;
+use Illuminate\Http\Request;
 
 final class PermanentlyHidePostController
 {
-    public function __invoke(Post $post)
+    public function __invoke(Post $post, Request $request)
     {
         $post->update([
             'hide_until' => now()->addYears(100)
         ]);
 
-        return redirect()->action(FindPostController::class);
+        return redirect()->action(FindPostController::class, ['filter' => $request->get('filter')]);
     }
 }
--- resources/views/includes/postAdmin.blade.php
+++ resources/views/includes/postAdmin.blade.php
@@ -75,7 +75,7 @@
 {{--                        </x-tag>--}}
 
                         <x-tag
-                            :url="action(\App\Http\Controllers\Posts\PermanentlyHidePostController::class, $post)"
+                            :url="action(\App\Http\Controllers\Posts\PermanentlyHidePostController::class, ['post' => $post, 'filter' => request()->get('filter')])"
                             color="red"
                         >
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 text-gray-500">
