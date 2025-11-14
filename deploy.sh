0bf08179fa402ff466d9629ec2efe27ac85363d5
brendt <brent.roose@gmail.com>
2025-11-14 14:51:19 +0100

    wip

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,7 @@ php8.4 /usr/local/bin/composer install --no-dev
 
 # Tempest
 php8.4 tempest cache:clear --force --internal --all
+php8.4 tempest view:clear
 php8.4 tempest discovery:generate
 php8.4 tempest migrate:up --force
 
